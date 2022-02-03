0ac4ff265880db357313002b95f431ffbe170524
gakkou0 <fhugo.625@gmail.com>
2022-02-03 16:33:07 -0300

    new push

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite sua nota: ");
+var nota = Console.ReadLine();
+
+Console.WriteLine("Sua nota é: " + nota);
