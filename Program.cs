f55c75db9287a40cb2fff61e71c5d2aaacc8add2
gakkou0 <fhugo.625@gmail.com>
2022-02-07 09:57:22 -0300

    Calculadora de média aritmética com número de valores variaveis

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Digite sua nota: ");
-var nota = Console.ReadLine();
+Console.WriteLine("Digite a quantide de valores: ");
 
-Console.WriteLine("Sua nota é: " + nota);
+int NumValores = int.Parse(Console.ReadLine());
+double[] Valores = new double[NumValores];
+
+//Console.WriteLine(NumValores);
+int i = 0;
+double SomaValores = 0;
+
+while(i < NumValores) {
+    Console.WriteLine("Digite o valor "+ (i+1) +":");
+    Valores[i] = double.Parse(Console.ReadLine());
+    
+    SomaValores = SomaValores + Valores[i];
+    i = i+1;
+}
+
+Console.WriteLine("Sua média é:");
+Console.WriteLine(SomaValores/NumValores);
